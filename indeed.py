a65ee81d557b9bda95440d95eba2f43c3d079691
NAMSEOK YOO <namseok.yoo@gmail.com>
2020-08-26 14:15:40 +0000

    ADD jobs title

---
+++ indeed.py
@@ -0,0 +1,33 @@
+# import urllib3
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&l=%EC%84%9C%EC%9A%B8&limit={LIMIT}"
+
+def extract_indeed_pages():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text,"html.parser")
+
+  pagination = soup.find("div",{"class":"pagination"})
+
+  links = pagination.find_all('a')
+  pages = []
+  for link in links[0:-1]:
+    pages.append(int(link.string))
+
+  max_page = pages[-1]
+  return max_page
+
+
+def extract_indeed_jobs(last_page):
+  jobs=[]
+  # for page in range(last_page):
+  result = requests.get(f"{URL}&start={0*LIMIT}")
+  soup = BeautifulSoup(result.text,"html.parser")
+  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+  for result in results:
+    title = result.find("h2",{"class":"title"}).find("a")["title"]
+    print(title)
+  return jobs
+    # print(result.status_code)
